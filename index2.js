7febfa514ee9ea974060933656e45f82da9cc17f
L.W.L <102495553@qq.com>
2019-04-15 00:42:20 +0800

    feat: 瀑布流懒加载

---
+++ index2.js
@@ -0,0 +1,102 @@
+~(function() {
+  const photoImg = document.getElementById('photoImg')
+  const imgCols = photoImg.getElementsByTagName('li')
+  const imgAll = photoImg.getElementsByTagName('img')
+
+  // 获取数据
+  function getImgData() {
+    const xhr = new XMLHttpRequest()
+
+    xhr.open('GET', './data.json', true)
+
+    xhr.onreadystatechange = function() {
+      if (this.readyState === 4 && /^2\d{2}$/.test(this.status)) {
+        const result = _utils.toJSON(this.responseText)
+        render(result)
+      }
+    }
+    xhr.send()
+  }
+
+  // 数据渲染
+  function render(data) {
+    data.forEach(item => {
+      const html = htmlStr(item)
+      const col = compareCols()
+      col.innerHTML += html
+    })
+  }
+
+  // 得到当前列中高度小的那个
+  function compareCols() {
+    const cols = Array.from(imgCols)
+    cols.sort((a, b) => {
+      return a.clientHeight - b.clientHeight
+    })
+    return cols[0]
+  }
+
+  // 生成html模板
+  function htmlStr(item) {
+    const {
+      id,
+      link,
+      src,
+      title,
+      height
+    } = item
+
+    return `
+      <a href="${link}" img-id="${id}">
+        <img
+          img-src="${src}"
+          src="./images/load.gif"
+          alt="${title}"
+          height="${height}"
+        />
+        <p class="title">${title}</p>
+      </a>
+    `
+  }
+
+  function loadMore() {
+    const scrollHeight = _utils.win('scrollHeight')
+    const scrollTop = _utils.win('scrollTop')
+    const clientHeight = _utils.win('clientHeight')
+    if (scrollTop + clientHeight >= scrollHeight - 100) {
+      console.log('到达底部了')
+      getImgData()
+    }
+  }
+
+  function lazyLoad() {
+    Array.from(imgAll).forEach(item => {
+      if (item.loaded) return;
+
+      const imgSrc = item.getAttribute('img-src')
+      const clientHeight = _utils.win('clientHeight')
+      const offsetHeight = item.offsetHeight
+      const scrollTop = _utils.win('scrollTop')
+      const { top } = _utils.offset(item)
+
+      if (clientHeight + scrollTop >= top + offsetHeight) {
+        let tempImg = new Image()
+        tempImg.src = imgSrc
+        tempImg.onload = function() {
+          item.src = imgSrc
+          item.loaded = true
+          tempImg = null
+        }
+      }
+
+    })
+  }
+
+  getImgData() // 获取初始数据 
+  lazyLoad() // 首次懒加载
+
+  window.onscroll = function () {
+    loadMore()
+    lazyLoad()
+  }
+})()
